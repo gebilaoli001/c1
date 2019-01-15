fe3571818c5ebd3ebdc9bbf1b831eb8f59eeb0ee
litiansuo <2296923560@qq.com>
2019-01-15 11:42:28 +0800

    将文字改为拼音

---
+++ c1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("jinruqianbaoxitong\n");
+    int qian = 0;
+
+    while (1)
+    {
+        printf("1---cunqian\n");
+        printf("2---quqian\n");
+        printf("3---yue\n");
+        printf("4---tuichu\n");
+        printf("qingshurugongnengbianhao\n");
+
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+    }
+
+    return 0;
+}
