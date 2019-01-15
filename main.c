8f03adb4172d4551cfc513ebac4e6b0d98576f06
litiansuo <2296923560@qq.com>
2019-01-15 11:27:43 +0800

    first init

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("进入钱包系统\n");
+    int qian = 0;
+
+    while (1)
+    {
+        printf("1---存钱\n");
+        printf("2---取钱\n");
+        printf("3---余额\n");
+        printf("4---退出\n");
+        printd("请输入功能编号\n");
+
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 1)
+        {
+        }
+        if (code == 1)
+        {
+        }
+        if (code == 1)
+        {
+        }
+    }
 
     return 0;
-
 }
